81d0626f1a1d0b70157922d895538fc6dc177844
marc1234wess-spec <marc1234wess@gmail.com>
2025-11-18 21:19:44 +0100

    Update DJV_flask_app.py

--- DJV_flask_app.py
+++ DJV_flask_app.py
@@ -1,1 +1,11 @@
-DJV_flask_app.py
+# Main
+if __name__ == '__main__':
+    import eventlet
+    import eventlet.wsgi
+    from werkzeug.middleware.proxy_fix import ProxyFix
+
+    # Render erwartet den Port in der ENV
+    port = int(os.environ.get("PORT", 5000))
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    print(f'Starte DJV auf Port {port}...')
+    socketio.run(app, host='0.0.0.0', port=port)
